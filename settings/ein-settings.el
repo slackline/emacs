23daece5e28dd6b696d948331dba59a8f3fc946d
slackline <nshephard@gmail.com>
2018-06-12 11:18:59 +0100

    commenting out delete-cell

--- settings/ein-settings.el
+++ settings/ein-settings.el
@@ -1,6 +1,6 @@
 ;;; Delete the current cell (WARNING! : No Undo)
-(define-key ein:notebook-mode-map "\C-c\C-d"
-              'ein:worksheet-delete-cell)
+;; (define-key ein:notebook-mode-map "\C-c\C-d"
+;;               'ein:worksheet-delete-cell)
 ;;; Turn off white-space deletion (causes warning messages https://goo.gl/X75omY)
 (add-hook 'ein:notebook-multilang-mode-hook
 	  #'(lambda () (spacemacs/toggle-whitespace-cleanup-off)))
