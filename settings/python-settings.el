3c82a544dc130caaaeabeffd37e73b294fda8b8e
slackline <nshephard@gmail.com>
2018-06-25 09:32:49 +0100

    Added custom pylint code

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -10,7 +10,11 @@
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
-
+;; Try to add pylint rules https://emacs.stackexchange.com/a/41048/10100
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (setq flycheck-python-pylint-executable "~/.local/bin/pylint")
+	    (setq flycheck-pylintrc "~/.emacs.d/settings/.pylintrc")))
 ;; enable autopep8 formatting on save
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
