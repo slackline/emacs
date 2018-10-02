ab306c92a6ec77709894133c1e3587577caedd72
slackline <nshephard@gmail.com>
2018-10-02 10:50:22 +0100

    enabling magit settings

--- init.el
+++ init.el
@@ -136,7 +136,7 @@
 ;; (split-window-vertically)
 ;; (R)
 (other-window 1)
-(term "/bin/zsh")
+;(term "/bin/zsh")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
