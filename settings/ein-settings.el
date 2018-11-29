c040fbd30f97b7cba5854cbafa7b760692fe651b
Neil Shephard <neil.shephard@583.datascience.thefloow.com>
2018-11-29 09:27:33 +0000

    Commenting out hook

--- settings/ein-settings.el
+++ settings/ein-settings.el
@@ -2,5 +2,6 @@
 ;; (define-key ein:notebook-mode-map "\C-c\C-d"
 ;;               'ein:worksheet-delete-cell)
 ;;; Turn off white-space deletion (causes warning messages https://goo.gl/X75omY)
-(add-hook 'ein:notebook-multilang-mode-hook
-	  (lambda () (emacs/toggle-whitespace-cleanup-off)))
+;;; Actual solution (https://emacs.stackexchange.com/a/40773/10100) added to init.el
+;;(add-hook 'ein:notebook-multilang-mode-hook
+;;	  (lambda () (emacs/toggle-whitespace-cleanup-off)))
