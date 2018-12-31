014a85d3953ee9e4dd870bf20c2fbe6affa3c44d
slackline <nshephard@gmail.com>
2018-12-31 09:15:44 +0000

    latex options

---
+++ settings/latex-settings.el
@@ -0,0 +1,5 @@
+;; LaTeX Extra (https://github.com/Malabarba/latex-extra)
+(add-hook 'LaTeX-mode-hook #'latex-extra-mode)
+
+;; Preview Pane (https://github.com/jsinglet/latex-preview-pane)
+(latex-preview-pane-enable)
