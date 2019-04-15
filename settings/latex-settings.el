a5bddd77183795f326b524959a80f68b76a89414
slackline <nshephard@gmail.com>
2019-04-15 12:01:33 +0100

    Added org-mode-reftex-setup function

--- settings/latex-settings.el
+++ settings/latex-settings.el
@@ -3,3 +3,15 @@
 
 ;; Preview Pane (https://github.com/jsinglet/latex-preview-pane)
 (latex-preview-pane-enable)
+
+;; RefTex setup
+;; From : https://blog.karssen.org/2013/08/22/using-bibtex-from-org-mode/
+;;        http://www.mfasold.net/blog/2009/02/using-emacs-org-mode-to-draft-papers/
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name)
+       (file-exists-p (buffer-file-name))
+       (reftex-parse-all))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
+  )
+(add-hook 'org-mode-hook 'org-mode-reftex-setup)
