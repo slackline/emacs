d7fb5b625dbd1b8a10eac0927b97e13735f0cf77
slackline <nshephard@gmail.com>
2019-05-13 08:57:48 +0100

    Added basic loading of mpdel and ivy-mpdel; still need to configure to work with servers

---
+++ settings/mpdel-settings.el
@@ -0,0 +1,8 @@
+;; MPDEL CONFIGURATION
+;; --------------------------------------
+(require 'mpdel)
+(mpdel-mode)
+(require 'ivy-mpdel)
+
+;; ToDo : Configure profiles to work with alarmpi and kimura
+
