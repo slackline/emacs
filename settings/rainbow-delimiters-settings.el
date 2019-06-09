4976d7397935874738c8360916d7a559c01b7f73
slackline <nshephard@gmail.com>
2019-06-09 07:26:00 +0100

    raindow-delimiter-settings.el added

---
+++ settings/rainbow-delimiters-settings.el
@@ -0,0 +1,6 @@
+;; RAINBOW DELIMITERS CONFIGURATION
+;; --------------------------------------
+;;
+;; https://github.com/Fanael/rainbow-delimiters
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
