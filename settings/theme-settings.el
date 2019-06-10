ff2098b3cf70529f2bf0377908821ff4563d2e55
Neil Shephard <neil.shephard@thefloow.com>
2019-06-10 09:29:09 +0100

    Selecting new theme

---
+++ settings/theme-settings.el
@@ -0,0 +1,24 @@
+;; THEME CONFIGURATION
+;; --------------------------------------
+;;
+;; I've tried out lots, one day I might settle on one.
+
+;; (load-theme 'darktooth t)
+;; (load-theme 'dracula t)
+
+;; (load-theme 'nova t)
+;; (load-theme 'material t)
+
+;; Kaolin
+(require 'kaolin-themes)
+;; (load-theme 'kaolin-dark t)         ;; a dark jade variant inspired by Sierra.vim.
+;; (load-theme 'kaolin-light t)        ;; light variant of the original kaolin-dark.
+(load-theme 'kaolin-aurora t)       ;; Kaolin meets polar lights.
+;; (load-theme 'kaolin-bubblegum t)    ;; Kaolin colorful theme with dark blue background.
+;; (load-theme 'kaolin-eclipse t)      ;; a dark purple variant.
+;; (load-theme 'kaolin-temple t)       ;; dark brown background with syntax highlighting based on orange and cyan shades.
+;; (load-theme 'kaolin-galaxy t)       ;; bright theme based on one of the Sebastian Andaur arts.
+;; (load-theme 'kaolin-ocean t)        ;; dark blue variant.
+;; (load-theme 'kaolin-valley-dark t)  ;; colorful Kaolin theme with brown background.
+;; (load-theme 'kaolin-valley-light t) ;; light version of kaolin-valley-dark theme.
+;; (load-theme 'kaolin-mono-dark t)    ;; almost monochrome dark green Kaolin theme.
