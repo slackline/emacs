382979b9f882f5cb5bbda7816dbdbbeb754fd5b7
Neil Shephard <neil.shephard@thefloow.com>
2019-06-13 10:53:55 +0100

    Setting auto-fill-mode for markdown files

---
+++ settings/markdown-settings.el
@@ -0,0 +1,8 @@
+;; PYTHON CONFIGURATION
+;; --------------------------------------
+
+;; Enable auto-fill-mode for Markdown files (https://emacs.stackexchange.com/a/46980/10100)
+
+(add-to-list 'auto-mode-alist
+	     '("\\.md\\'" . auto-fill-mode))
+
