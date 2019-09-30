d8afa74fa7c9e36d62038e4b7a740a68e80e6610
Neil Shephard <neil.shephard@thefloow.com>
2019-09-30 09:41:13 +0100

    Added another dark theme!

--- settings/theme-settings.el
+++ settings/theme-settings.el
@@ -2,6 +2,7 @@
 ;; --------------------------------------
 ;;
 ;; I've tried out lots, one day I might settle on one.
+(require 'kaolin-themes)
 
 ;; (load-theme 'darktooth t)
 ;; (load-theme 'dracula t)
@@ -9,16 +10,21 @@
 ;; (load-theme 'nova t)
 ;; (load-theme 'material t)
 
-;; Kaolin
-(require 'kaolin-themes)
 ;; (load-theme 'kaolin-dark t)         ;; a dark jade variant inspired by Sierra.vim.
+;; (load-theme 'kaolin-eclipse t)      ;; a dark purple variant.
+;; (load-theme 'kaolin-valley-dark t)  ;; colorful Kaolin theme with brown background.
+
+(load-theme 'ample-zen t)
+
+;; Kaolin
+
 ;; (load-theme 'kaolin-light t)        ;; light variant of the original kaolin-dark.
 ;; (load-theme 'kaolin-aurora t)       ;; Kaolin meets polar lights.
 ;; (load-theme 'kaolin-bubblegum t)    ;; Kaolin colorful theme with dark blue background.
-;; (load-theme 'kaolin-eclipse t)      ;; a dark purple variant.
+
 ;; (load-theme 'kaolin-temple t)       ;; dark brown background with syntax highlighting based on orange and cyan shades.
 ;; (load-theme 'kaolin-galaxy t)       ;; bright theme based on one of the Sebastian Andaur arts.
 ;; (load-theme 'kaolin-ocean t)        ;; dark blue variant.
-(load-theme 'kaolin-valley-dark t)  ;; colorful Kaolin theme with brown background.
+
 ;; (load-theme 'kaolin-valley-light t) ;; light version of kaolin-valley-dark theme.
 ;; (load-theme 'kaolin-mono-dark t)    ;; almost monochrome dark green Kaolin theme.
