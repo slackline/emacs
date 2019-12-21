2e3f771663e3557eee900818dc56d4eadb36ed7e
slackline <nshephard@gmail.com>
2019-12-21 23:12:34 +0000

    org-present configuration

--- settings/org-mode-settings.el
+++ settings/org-mode-settings.el
@@ -29,3 +29,21 @@
   (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
   )
 (add-hook 'org-mode-hook 'org-mode-reftex-setup)
+
+;; org-present
+(autoload 'org-present "org-present" nil t)
+
+(eval-after-load "org-present"
+  '(progn
+     (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (org-present-big)
+                 (org-display-inline-images)
+                 (org-present-hide-cursor)
+                 (org-present-read-only)))
+     (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 (org-present-show-cursor)
+                 (org-present-read-write)))))
