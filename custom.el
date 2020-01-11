44967fccca79134989b9801d3934ec2faad8013a
slackline <nshephard@gmail.com>
2020-01-11 16:43:47 +0000

    Adding keychain-environment settings

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(ein:polymode t)
  '(package-selected-packages
    (quote
-    (modus-vivendi-theme gnu-elpa-keyring-update yapfify ample-zen-theme auto-compile kaolin-themes latex-extra json-mode rainbow-delimiters org-cliplink powerline ivy-mpdel nova-theme dracula-theme jedi latex-preview-pane py-yapf blacken gitlab mmm-mode python-pytest python-mode pytest ess-smart-underscore magit projectile-git-autofetch projectile poly-R poly-markdown poly-org elpy yasnippet yaml-mode wide-column vimish-fold pyvenv pylint py-autopep8 polymode package-utils package+ org-time-budgets material-theme julia-mode highlight-parentheses highlight-indentation flycheck find-file-in-project eink-theme ein darktooth-theme company better-defaults autopair auto-package-update pass password-store-otp password-store org-parser ht f)))
+    (keychain-environment pygen forge org-present modus-vivendi-theme gnu-elpa-keyring-update yapfify ample-zen-theme auto-compile kaolin-themes latex-extra json-mode rainbow-delimiters org-cliplink powerline ivy-mpdel nova-theme dracula-theme jedi latex-preview-pane py-yapf blacken gitlab mmm-mode python-pytest python-mode pytest ess-smart-underscore magit projectile-git-autofetch projectile poly-R poly-markdown poly-org elpy yasnippet yaml-mode wide-column vimish-fold pyvenv pylint py-autopep8 polymode package-utils package+ org-time-budgets material-theme julia-mode highlight-parentheses highlight-indentation flycheck find-file-in-project eink-theme ein darktooth-theme company better-defaults autopair auto-package-update pass password-store-otp password-store org-parser ht f)))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
@@ -35,4 +35,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
---
+++ settings/keychain-environment-settings.el
@@ -0,0 +1,3 @@
+;; KEYCHAIN CONFIGURATION
+;; --------------------------------------
+(keychain-refresh-environment)
