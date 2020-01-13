b4342414fe19d0b4f8292f7b02febc2e6bd59afb
slackline <nshephard@gmail.com>
2020-01-13 14:50:14 +0000

    Adding some defaults for tramp ssh processes

--- settings/tramp-settings.el
+++ settings/tramp-settings.el
@@ -2,3 +2,11 @@
 ;; --------------------------------------
 
 (setq tramp-default-method "ssh")
+
+;; Set default usernames for different hosts and a global default.
+(add-to-list 'tramp-default-user-alist
+             '("ssh" ".*ovh'" "arch"))
+(add-to-list 'tramp-default-user-alist
+             '("ssh" ".*openwrt" "admin") t)
+(add-to-list 'tramp-default-user-alist
+             '(nil nil "neil") t)
