69e18343afc5f23551eb5daa7a68e3521eb5b29e
slackline <nshephard@gmail.com>
2020-09-07 07:52:13 +0100

    Correctly defining host and port \o/

--- settings/mpdel-settings.el
+++ settings/mpdel-settings.el
@@ -4,6 +4,6 @@
 (mpdel-mode)
 (require 'ivy-mpdel)
 
-;; ToDo : Configure profiles to work with alarmpi and kimura
-(defvar libmpdel-hostname "192.168.1.21")
-(defvar libmpdel-port 6600)
+;; Alarmpi control
+(setq libmpdel-hostname "192.168.1.21")
+(setq libmpdel-port 6600)
