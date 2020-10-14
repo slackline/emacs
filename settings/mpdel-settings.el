c7cbda48c58e823092bbd5fb4b92a455815c258b
slackline <nshephard@gmail.com>
2020-10-14 09:11:06 +0100

    Moving setting of variables to within use-package

--- settings/mpdel-settings.el
+++ settings/mpdel-settings.el
@@ -1,11 +1,10 @@
 ;; MPDEL CONFIGURATION
 ;; --------------------------------------
 (use-package mpdel
-  :defer 2)
+  :defer 2
+  :init
+  (setq libmpdel-hostname "192.168.1.21")
+  (setq libmpdel-port 6600))
 ;; (mpdel-mode)
 (use-package ivy-mpdel
   :defer 2)
-
-;; Alarmpi control
-(setq libmpdel-hostname "192.168.1.21")
-(setq libmpdel-port 6600)
