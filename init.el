6a8503e46ec73e972f50ed55e60c99ff7ea8775e
slackline <nshephard@gmail.com>
2020-10-14 08:56:16 +0100

    Disabling splitting of window on start up which is redundant now I'm using server mode

--- init.el
+++ init.el
@@ -191,10 +191,10 @@
 ;;; Split the window and start an R session
 ;; (split-window-horizontally)
 ;; (other-window 1)
-(split-window-vertically)
+;; (split-window-vertically)
 ;; (R)
-(other-window 1)
-(term "/bin/zsh")
+;; (other-window 1)
+;; (term "/bin/zsh")
 
 
 (setq custom-file "~/.config/emacs/custom.el")
