51593707be9103674b5c6e05dcb04aa6dc8163b2
slackline <nshephard@gmail.com>
2020-10-14 08:58:15 +0100

    Switching initialisation to work correctly with use-package

--- settings/autopair-settings.el
+++ settings/autopair-settings.el
@@ -1,4 +1,5 @@
 ;;; Autopair.el for automatic parentheses and quote matching
 (use-package autopair
-  :defer 1)
-(autopair-global-mode) ;; enable autopair in all buffers
+  :defer 1
+  :init
+  (autopair-global-mode)) ;; enable autopair in all buffers
