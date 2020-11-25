aca4693e63de21b775f12ac3f4fe435f35b8cc8f
slackline <nshephard@gmail.com>
2020-11-25 09:48:15 +0000

    Adding more refinements to TRAMP configuration

--- settings/tramp-settings.el
+++ settings/tramp-settings.el
@@ -3,6 +3,8 @@
 
 (setq tramp-default-method "ssh")
 
+(add-to-list 'tramp-default-method-alist '("" "neil" "ssh"))
+
 ;; Set default usernames for different hosts and a global default.
 (add-to-list 'tramp-default-user-alist
              '("ssh" ".*ovh'" "arch"))
@@ -10,3 +12,5 @@
              '("ssh" ".*openwrt" "admin") t)
 (add-to-list 'tramp-default-user-alist
              '(nil nil "neil") t)
+(add-to-list 'tramp-default-user-alist
+             '("ssh" ".*alarmpi-4b" "neil") t)
