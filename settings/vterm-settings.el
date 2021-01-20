b2604066256b2872db0a2d11487e6738203c3ac2
slackline <nshephard@gmail.com>
2021-01-20 06:52:53 +0000

    Setting custom variables for vterm

--- settings/vterm-settings.el
+++ settings/vterm-settings.el
@@ -2,4 +2,8 @@
 ;; --------------------------------------
 ;; https://github.com/akermu/emacs-libvterm
 (use-package vterm
-    :ensure t)
+  :ensure t
+  :config
+  (setq vterm-always-compile-module t
+	vterm-copy-exclude-prompt t
+	vterm-kill-buffer-on-exit t))
