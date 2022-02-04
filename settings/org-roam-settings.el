76d3f5d5c9e22ca69ce729a6347074d0964ac6ca
slackline <nshephard@gmail.com>
2022-02-04 08:01:22 +0000

    Missing closing bracket fixed

--- settings/org-roam-settings.el
+++ settings/org-roam-settings.el
@@ -8,19 +8,30 @@
   :init
   (setq org-roam-v2-ack t)
   :custom
-  (org-roam-directory "~/org/roam")
+  (org-roam-directory "~/work/org-roam")
+  (org-roam-dailies-directory "daily/")
+  (org-roam-db-autosync-mode)
   (org-roam-completion-everywhere t)
   (org-roam-capture-templates
    '(("d" "default" plain
       "%?"
-      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
       :unnarrowed t)))
+  (org-roam-dailies-capture-templates
+      '(("d" "default" entry
+         "* %?"
+         :target (file+head "%<%Y-%m-%d>.org"
+                            "#+title: %<%Y-%m-%d>\n"))))
   :hook
   (after-init . org-roam-mode)
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
+  :bind (("C-c n a" . org-roam-alias-add)
+	 ("C-c n c" . org-roam-capture)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
          ("C-c n g" . org-roam-graph-show)
-	 ("C-c n i" . org-roam-node-insert))
+	 ("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n o" . org-id-get-create)
+	 ("C-c n t" . org-tag-add))
   :config
   (org-roam-setup))
 
