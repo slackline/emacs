e7244a2819d5380f54771c5d8e94cd71d7f1d1f4
slackline <nshephard@gmail.com>
2022-02-13 15:45:27 +0000

    Ensuring settings/key-bindings.el is called

--- init.el
+++ init.el
@@ -158,6 +158,7 @@
 ;;; General editor settings
 ;; (load "~/.config/emacs/settings/auto-package-update.el")
 (load "~/.config/emacs/settings/centaur-tabs-settings.el")
+(load "~/.config/emacs/settings/key-bindings.el")
 (load "~/.config/emacs/settings/highlight-indent-guides-settings.el")
 (load "~/.config/emacs/settings/highlight-parentheses-settings.el")
 (load "~/.config/emacs/settings/rainbow-delimiters-settings.el")
