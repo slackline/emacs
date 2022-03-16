47a4f6974f1b12a3892791585747469281c4d758
slackline <nshephard@gmail.com>
2022-03-16 06:31:52 +0000

    Adding osm-settings.el; org-roam.db update

---
+++ settings/osm-settings.el
@@ -0,0 +1,18 @@
+;;; OSM CONFIGURATION
+;;; --------------------------------------
+;;;
+;;; https://elpa.gnu.org/packages/osm.html#org666a5ba
+(use-package osm
+  :bind (("C-c m h" . osm-home)
+	 ("C-c m s" . osm-search)
+	 ("C-c m v" . osm-server)
+	 ("C-c m t" . osm-goto)
+	 ("C-c m x" . osm-gpx-show)
+	 ("C-c m j" . osm-bookmark-jump))
+  :init
+  ;; Load Org link support
+  (with-eval-after-load 'org
+    (require 'osm-ol))
+  :config
+  ;;(setq osm-home (53.35611 -1.46326 15))
+  )
