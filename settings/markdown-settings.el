c5d27fcb7cb17b4ffa9273c2fea106dc60e0e8e9
slackline <nshephard@gmail.com>
2022-04-14 07:04:23 +0100

    finally corrects an error in loading markdown-mode

--- settings/markdown-settings.el
+++ settings/markdown-settings.el
@@ -3,8 +3,8 @@
 ;; https://github.com/jrblevin/markdown-mode
 (use-package markdown-mode
   :ensure t
-  :mode ("\\.md\\'" . markdown-mode)
-  :hook (markdown-mode .auto-fill-mode)
+  :mode ("\\.md\\'" . gfm-mode)
+  :hook (markdown-mode . auto-fill-mode)
   :init (setq markdown-command "pandoc"))
 
 
