ee35fba4fd6ab9753a2c5062b4efa191aefe148c
slackline <nshephard@gmail.com>
2022-05-11 08:13:56 +0100

    Enabling git-modes

--- settings/magit-settings.el
+++ settings/magit-settings.el
@@ -2,11 +2,28 @@
 ;; --------------------------------------
 (use-package magit
   :defer 0.5
+  :requires forge
   :init
   (setq magit-repository-directories
       `(("~/dotfiles" . 1)
-	("~/.config/emacs/" . 1)
-	("~/org/" . 1))))
+	    ("~/.config/emacs/" . 1)
+	    ("~/org/" . 1)
+	    ("~/work/org-roam/" . 1)
+	    ("~/work/python/tcx2gpx/" . 1)
+	    ("~/work/python/wpweather/" . 1)
+        ))
+   (setq auth-sources '("~/.authinfo.gpg"))
+  :config
+  (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
 
+;; FORGE CONFIGURATION
+;; --------------------------------------
+;; https://magit.vc/manual/ghub.html#Getting-Started
+;; https://magit.vc/manual/forge.html#Getting-Started
 ;; Automatically refresh buffer
-(add-hook 'after-save-hook 'magit-after-save-refresh-status t)
+;; (add-hook 'after-save-hook 'magit-after-save-refresh-status t)
+
+;; Git modes
+(use-package git-modes
+  :defer t
+  :ensure t)
