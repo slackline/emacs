7ab5638b060dc92587ad59524fd56c569e268601
Neil Shephard <n.shephard@sheffield.ac.uk>
2022-06-24 12:46:41 +0100

    Tweaks to centaur tabs

--- settings/centaur-tabs-settings.el
+++ settings/centaur-tabs-settings.el
@@ -1,5 +1,6 @@
-;; CONDA CONFIGURATION
+;; CENTAUR TABS CONFIGURATION
 ;; --------------------------------------
+;; https://github.com/ema2159/centaur-tabs
 (use-package centaur-tabs
   :demand
   :config
@@ -9,9 +10,10 @@
   (setq centaur-tabs-set-icons t)
   (setq centaur-tabs-set-bar 'under)
   (setq x-underline-at-descent-line t)
+  (setq centaur-tabs-cycle-scope 'default)
   :bind
-  ("C-<prior>" . centaur-tabs-backward)
-  ("C-<next>" . centaur-tabs-forward))
+  ("C-<tab> p" . centaur-tabs-backward)
+  ("C-<tab> n" . centaur-tabs-forward))
 
 (defun centaur-tabs-buffer-groups ()
   "`centaur-tabs-buffer-groups' control buffers' group rules.
@@ -25,31 +27,62 @@
      "Terminal")
     ((or (string-equal "*" (substring (buffer-name) 0 1))
 	 (memq major-mode '(magit-process-mode
-			    magit-status-mode
-			    magit-diff-mode
-			    magit-log-mode
-			    magit-file-mode
-			    magit-blob-mode
-			    magit-blame-mode
-			    )))
+			            magit-status-mode
+			            magit-diff-mode
+			            magit-log-mode
+			            magit-file-mode
+			            magit-blob-mode
+			            magit-blame-mode
+			            )))
      "Emacs")
     ((derived-mode-p 'prog-mode)
      "Editing")
+    ((derived-major-mode 'python-mode)
+     "Python")
     ((derived-mode-p 'dired-mode)
      "Dired")
     ((memq major-mode '(helpful-mode
 			help-mode))
      "Help")
     ((memq major-mode '(org-mode
-			org-agenda-clockreport-mode
-			org-src-mode
-			org-agenda-mode
-			org-beamer-mode
-			org-indent-mode
-			org-bullets-mode
-			org-cdlatex-mode
-			org-agenda-log-mode
-			diary-mode))
+			            org-agenda-clockreport-mode
+			            org-src-mode
+			            org-agenda-mode
+			            org-beamer-mode
+			            org-indent-mode
+			            org-bullets-mode
+			            org-cdlatex-mode
+			            org-agenda-log-mode
+			            diary-mode))
      "OrgMode")
     (t
      (centaur-tabs-get-group-name (current-buffer))))))
+
+(defun centaur-tabs-hide-tab (x)
+  "Do no to show buffer X in tabs."
+  (let ((name (format "%s" x)))
+    (or
+     ;; Current window is not dedicated window.
+     (window-dedicated-p (selected-window))
+
+     ;; Buffer name not match below blacklist.
+     (string-prefix-p "*epc" name)
+     (string-prefix-p "*helm" name)
+     (string-prefix-p "*Helm" name)
+     (string-prefix-p "*Compile-Log*" name)
+     (string-prefix-p "*lsp" name)
+     (string-prefix-p "*company" name)
+     (string-prefix-p "*Flycheck" name)
+     (string-prefix-p "*tramp" name)
+     (string-prefix-p " *Mini" name)
+     (string-prefix-p "*help" name)
+     (string-prefix-p "*straight" name)
+     (string-prefix-p " *temp" name)
+     (string-prefix-p "*Help" name)
+     (string-prefix-p "*mybuf" name)
+
+     ;; Is not magit buffer.
+     ;; (and (string-prefix-p "magit" name)
+	 ;;  (not (file-name-extension name)))
+     ;; ))
+  )
