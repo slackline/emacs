19eb6f515b080b06ddcd2bfae828964c5da2c97d
slackline <nshephard@protonmail.com>
2023-01-15 13:54:47 +0000

    Link for configuring tree-sitter under 29; reverting org-capture

--- settings/tree-sitter-settings.el
+++ settings/tree-sitter-settings.el
@@ -4,7 +4,8 @@
 ;;; Homepage    : https://emacs-tree-sitter.github.io/installation/
 ;;; GitHub      : https://github.com/emacs-tree-sitter/elisp-tree-sitter
 ;;; Tree-Sitter : https://tree-sitter.github.io/tree-sitter/
-
+;;;
+;;; https://wwwtech.de/articles/2022/dec/emacs-29-install-tree-sitter-parser-modules-with-a-minor-mode
 (use-package tree-sitter
 	     :ensure t
 	     :init
