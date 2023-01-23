b99f08f50c86211a155bd5aec4204a5df7da5e07
Neil Shephard <n.shephard@sheffield.ac.uk>
2023-01-23 21:31:17 +0000

    Trying out mingus

---
+++ settings/mingus-settings.el
@@ -0,0 +1,10 @@
+;;; Mingus CONFIGURATION
+;;; --------------------------------------
+;;;
+;;; GitHub : https://github.com/pft/mingus
+
+(use-package mingus
+  :defer t
+  :config
+  (setq mingus-mpd-host '192.168.1.28
+        mingus-mpd-port '6600))
