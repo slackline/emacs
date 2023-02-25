1738cf5d84c9dcb03f0b724b9ba49cf9a6d9b736
slackline <nshephard@protonmail.com>
2023-02-25 21:15:33 +0000

    links for treesitter config

--- settings/tree-sitter-settings.el
+++ settings/tree-sitter-settings.el
@@ -4,14 +4,16 @@
 ;;; Homepage    : https://emacs-tree-sitter.github.io/installation/
 ;;; GitHub      : https://github.com/emacs-tree-sitter/elisp-tree-sitter
 ;;; Tree-Sitter : https://tree-sitter.github.io/tree-sitter/
+;;; ts-fold     : https://github.com/emacs-tree-sitter/ts-fold
 ;;;
 ;;; https://wwwtech.de/articles/2022/dec/emacs-29-install-tree-sitter-parser-modules-with-a-minor-mode
 ;;; https://archive.casouri.cc/note/2023/tree-sitter-starter-guide/index.html
 ;;; https://archive.casouri.cc/note/2023/tree-sitter-in-emacs-29/index.html
 ;;; https://www.reddit.com/r/emacs/comments/r0i031/comment/hlxwhyu/
-;;;
+;;; https://www.youtube.com/watch?v=MZPR_SC9LzE
 ;;; https://mastodon.social/@spaceotter@mastodon.online/109671596235038019
 ;;; https://mastodon.social/@spaceotter@mastodon.online/109774686483526367
+;;;
 ;;; Extensions
 ;;;
 ;;; https://github.com/erickgnavar/tree-sitter-ispell.el
