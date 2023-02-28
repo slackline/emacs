ba4d8657ae5269eae3e5e51c316b2e96ba7ee93c
slackline <nshephard@protonmail.com>
2023-02-28 22:26:23 +0000

    Tidying up clutter from markdown-settings.el

--- settings/markdown-settings.el
+++ settings/markdown-settings.el
@@ -6,8 +6,3 @@
 	     :mode ("\\.md\\'" . gfm-mode)
 	     :hook (markdown-mode . auto-fill-mode)
 	     :init (setq markdown-command "pandoc"))
-
-
-;; Enable auto-fill-mode for Markdown files (https://emacs.stackexchange.com/a/46980/10100)
-;; (add-to-list 'auto-mode-alist
-;;      '("\\.md\\'" . auto-fill-mode))
