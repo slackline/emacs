e8ac429f6490a0fab849045b10c5a6a9c7ddf8ce
slackline <nshephard@protonmail.com>
2023-03-30 09:54:17 +0100

    Ensuring keychain-environment is installed via use-package

--- settings/keychain-environment-settings.el
+++ settings/keychain-environment-settings.el
@@ -2,5 +2,7 @@
 ;; ----------------------
 ;;
 ;; GitHub : https://github.com/tarsius/keychain-environment
+(use-package keychain-environment
+  :ensure t)
 (keychain-refresh-environment)
 (global-set-key (kbd "C-c k") 'keychain-refresh-environment)
