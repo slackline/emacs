8f2a4c348fe84916254df4255703ca699d448117
slackline <nshephard@protonmail.com>
2023-05-04 09:22:40 +0100

    Adding work org-capture hooks and rainbow-mode

--- settings/rainbow-delimiters-settings.el
+++ settings/rainbow-delimiters-settings.el
@@ -2,3 +2,10 @@
 ;; --------------------------------------
 ;; https://github.com/Fanael/rainbow-delimiters
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(use-package rainbow-mode
+	     :ensure t
+	     :defer 1
+  	     :hook
+	     (prog-mode-hook . rainbow-mode)
+	     )
