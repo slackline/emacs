7afd0322ec6c0ab6570f01085ff5da1059e97d02
Neil Shephard <n.shephard@sheffield.ac.uk>
2023-05-17 23:44:16 +0100

    Adding mermaid packages/config

---
+++ settings/mermaid-mode.el
@@ -0,0 +1,8 @@
+;;; Mermaid Mode
+;;; https://github.com/abrochard/mermaid-mode/
+(use-package mermaid-mode
+	     ensure: t
+	     defer: t)
+(use-package ob-mermaid
+	     ensure: t
+	     defer: t)
