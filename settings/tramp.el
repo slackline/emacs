ca369df261bc585549e073a584d2d325907b6532
slackline <nshephard@protonmail.com>
2023-08-15 08:42:56 +0100

    Improving TRAMP config so its actually loaded!

--- settings/tramp.el
+++ settings/tramp.el
@@ -3,19 +3,20 @@
 ;; https://emacs.stackexchange.com/questions/47969/trouble-connecting-gnu-emacs-to-work-machine-through-ssh-tramp
 ;; https://emacs.stackexchange.com/a/48089
 
-(setq tramp-default-method "ssh")
-
-(add-to-list 'tramp-default-method-alist '("" "neil" "ssh"))
-
-;; Set prompt so it doesn't hang
-(setq shell-prompt-pattern '"^[^#$%>\n]*~?[#$%>] *")
-
-;; Set default usernames for different hosts and a global default.
-(add-to-list 'tramp-default-user-alist
-             '("ssh" ".*ovh'" "arch") t)
-(add-to-list 'tramp-default-user-alist
-             '("ssh" ".*openwrt" "admin") t)
-(add-to-list 'tramp-default-user-alist
-             '(nil nil "neil") t)
-(add-to-list 'tramp-default-user-alist
-             '("ssh" ".*alarmpi-4b" "neil") t)
+(use-package tramp
+  :ensure t
+  :config
+  (setq tramp-default-method "ssh")
+  (add-to-list 'tramp-default-method-alist '("" "neil" "ssh"))
+  ;; Set prompt so it doesn't hang
+  (setq shell-prompt-pattern '"^[^#$%>\n]*~?[#$%>] *")
+  ;; Set default usernames for different hosts and a global default.
+  (add-to-list 'tramp-default-user-alist
+	       '("ssh" ".*ovh'" "arch") t)
+  (add-to-list 'tramp-default-user-alist
+	       '("ssh" ".*openwrt" "admin") t)
+  (add-to-list 'tramp-default-user-alist
+	       '(nil nil "neil") t)
+  (add-to-list 'tramp-default-user-alist
+	       '("ssh" ".*alarmpi-4b" "neil") t)
+  )
