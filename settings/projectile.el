2511a2d2b8a413f6cffa7cbb907c5b0b913f8796
Neil Shephard <n.shephard@sheffield.ac.uk>
2023-09-20 12:27:49 +0100

    Improving projectile settings

--- settings/projectile.el
+++ settings/projectile.el
@@ -1,10 +1,16 @@
 ;;; PROJECTILE CONFIGURATION
 ;;; ------------------------
 ;;; https://github.com/bbatsov/projectile/
+;;; https://docs.projectile.mx/
 (use-package projectile
   :ensure t
   :defer 3
+  :init
+  (projectile-mode +1)
   :bind
-  ("C-c C-p" . projectile-command-map)
+  (:map projectile-mode-map
+        ("s-p" . projectile-command-map)
+        ("C-c P" . projectile-command-map))
   :config
-  (setq projectile-project-search-path '("~/org/" "~/.config/emacs" ("~/work/git/" . 1))))
+  (setq projectile-project-search-path '("~/org/" "~/org-roam" "~/.config/emacs" ("~/work/git/" . 1)))
+  (setq projectile-enable-caching t))
