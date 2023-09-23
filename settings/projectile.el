d03c5771bb4e93ad95bdd61ec38fd426c1d4bb0a
slackline <nshephard@protonmail.com>
2023-09-23 22:13:23 +0100

    Remapping projectile-command-map

--- settings/projectile.el
+++ settings/projectile.el
@@ -9,8 +9,7 @@
   (projectile-mode +1)
   :bind
   (:map projectile-mode-map
-        ("s-p" . projectile-command-map)
-        ("C-c P" . projectile-command-map))
+        ("s-p" . projectile-command-map))
   :config
   (setq projectile-project-search-path '("~/org/" "~/org-roam" "~/.config/emacs" ("~/work/git/" . 2)))
   (setq projectile-enable-caching t))
