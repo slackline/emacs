6d35b2f6ee1adaebedfc33d73a2a5cffe124f094
slackline <nshephard@protonmail.com>
2023-10-02 06:38:13 +0100

    Adding mermaid.el

--- settings/mermaid-mode.el
+++ settings/mermaid-mode.el
@@ -1,8 +1,12 @@
 ;;; Mermaid Mode
 ;;; https://github.com/abrochard/mermaid-mode/
-(use-package mermaid-mode
-	     ensure: t
-	     defer: t)
+;; (use-package mermaid-mode
+;; 	     :ensure t
+;; 	     :defer t)
 (use-package ob-mermaid
-	     ensure: t
-	     defer: t)
+  :ensure t
+  :defer t)
+;;; https://github.com/JonathanHope/mermaid-ts-mode
+(use-package mermaid-ts-mode
+  :ensure t
+  :defer t)
