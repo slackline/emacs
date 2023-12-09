a38ca35c1ea07ab9d1c7dd9088b0bdeb4905c1f0
slackline <nshephard@protonmail.com>
2023-12-09 18:10:55 +0000

    Adding dired-rsync to packages

--- settings/dirvish.el
+++ settings/dirvish.el
@@ -1,41 +1,51 @@
 ;;; Dirvish Settings
 ;;; https://github.com/alexluigit/dirvish
 (use-package dirvish
-	     :ensure t
-	     :defer 3
-	     :init
-	     (dirvish-override-dired-mode)
-	     :config
-	     (setq dirvish-attributes
-		   '(vc-state subtree-state all-the-icons collapse git-msg file-time file-size))
-	     ;; Placement
-	     ;; (setq dirvish-use-header-line nil)     ; hide header line (show the classic dired header)
-	     ;; (setq dirvish-use-mode-line nil)       ; hide mode line
-	     (setq dirvish-use-header-line 'global)    ; make header line span all panes
+  :ensure t
+  :defer 3
+  :init
+  (dirvish-override-dired-mode)
+  :config
+  (setq dirvish-attributes
+	'(vc-state subtree-state all-the-icons collapse git-msg file-time file-size))
+  ;; Placement
+  ;; (setq dirvish-use-header-line nil)     ; hide header line (show the classic dired header)
+  ;; (setq dirvish-use-mode-line nil)       ; hide mode line
+  (setq dirvish-use-header-line 'global)    ; make header line span all panes
 
-	     ;; Height
+  ;; Height
   ;;; '(25 . 35) means
   ;;;   - height in single window sessions is 25
   ;;;   - height in full-frame sessions is 35
-	     (setq dirvish-header-line-height '(25 . 35))
-	     (setq dirvish-mode-line-height 25) ; shorthand for '(25 . 25)
+  (setq dirvish-header-line-height '(25 . 35))
+  (setq dirvish-mode-line-height 25) ; shorthand for '(25 . 25)
 
-	     ;; Segments
+  ;; Segments
   ;;; 1. the order of segments *matters* here
   ;;; 2. it's ok to place raw string inside
-	     (setq dirvish-header-line-format
-		   '(:left (path) :right (free-space))
-		   dirvish-mode-line-format
-		   '(:left (sort file-time " " file-size symlink) :right (omit yank index))))
+  (setq dirvish-header-line-format
+	'(:left (path) :right (free-space))
+	dirvish-mode-line-format
+	'(:left (sort file-time " " file-size symlink) :right (omit yank index))))
 
 (use-package all-the-icons
-	     :ensure t
-	     :after dirvish)
+  :ensure t
+  :after dirvish)
 
 (use-package all-the-icons-dired
-	     :ensure t
-	     :defer t)
+  :ensure t
+  :defer t)
 
 (use-package all-the-icons-ibuffer
-	     :ensure t
-	     :defer t)
+  :ensure t
+  :defer t)
+
+;; https://github.com/stsquad/dired-rsync
+(use-package dired-rsync
+  :ensure t
+  :defer t)
+
+;; https://github.com/stsquad/dired-rsync
+(use-package dired-rsync-transient
+  :ensure t
+  :defer t)
