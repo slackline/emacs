527060f9713a11befaeec621f1a0c5405d99d930
slackline <nshephard@protonmail.com>
2024-01-26 11:51:06 +0000

    Adding .cff to yaml-mode

--- settings/yaml.el
+++ settings/yaml.el
@@ -2,4 +2,5 @@
 ;; --------------------------------------
 (use-package yaml-mode
   :init
-  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.cff\\'" . yaml-mode)))
