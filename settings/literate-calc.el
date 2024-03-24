44c93eb64a5ac715dc975283557dcf12e758a7f1
slackline <nshephard@protonmail.com>
2024-03-24 23:25:48 +0000

    Adding casual calc-mode (but not enabling)

--- settings/literate-calc.el
+++ settings/literate-calc.el
@@ -2,3 +2,8 @@
 ;; --------------------------------------
 (use-package literate-calc-mode
   :ensure t)
+
+;; http://yummymelon.com/devnull/announcing-casual-an-opinionated-porcelain-for-emacs-calc.html
+;; (use-package casual
+;;   :bind (:map calc-mode-map
+;;           ("C-o" . casual-main-menu)))
