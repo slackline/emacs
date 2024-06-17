83a2c4e8863ef09489a5e496ad11a07c1433f256
slackline <nshephard@protonmail.com>
2024-06-17 06:06:33 +0100

    casual: Updating casual programmes & anticipating casual-suite

--- settings/casual-suite.el
+++ settings/casual-suite.el
@@ -2,3 +2,9 @@
 ;;; http://yummymelon.com/devnull/announcing-a-plan-to-change-some-casual-package-names.html
 (use-package casual-suite
   :ensure t)
+
+(use-package casual-isearch
+  :ensure t)
+
+(use-package casual-lib
+  :ensure t)
