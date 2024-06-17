8bb060c0b07454ea66f470938066b59fc14c5835
slackline <nshephard@protonmail.com>
2024-06-17 06:07:15 +0100

    orgmode: org-clock-persist option set to history

--- settings/org-mode.el
+++ settings/org-mode.el
@@ -39,6 +39,7 @@
 	org-agenda-include-diary t
 	org-agenda-skip-deadline-if-done t
 	org-agenda-skip-scheduled-if-done t
+	org-clock-persist 'history                                   ;; https://anonimno.codeberg.page/tracking%20worktime.html
 	org-log-done 'time
 	org-image-actual-width nil                                   ;; https://stackoverflow.com/a/38477233/1444043
 	org-export-backends '(beamer html latex md odt)
