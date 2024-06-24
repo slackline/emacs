d02de8f00b295c56c20e0806f9cfe6cebd602cee
slackline <nshephard@protonmail.com>
2024-06-24 20:05:45 +0100

    Casual suite added

--- settings/casual-suite.el
+++ settings/casual-suite.el
@@ -1,10 +1,25 @@
 ;;; CASUAL SUITE
-;;; http://yummymelon.com/devnull/announcing-a-plan-to-change-some-casual-package-names.html
+;;; http://yummymelon.com/devnull/announcing-casual-suite.html
+;;; https://github.com/kickingvegas/casual-suite
 (use-package casual-suite
   :ensure t)
 
+;; https://github.com/kickingvegas/casual-isearch
 (use-package casual-isearch
   :ensure t)
 
-(use-package casual-lib
+;; https://github.com/kickingvegas/casual-dired
+(use-package casual-dired
+  :ensure t)
+
+;; https://github.com/kickingvegas/casual-info
+(use-package casual-dired
+  :ensure t)
+
+;; https://github.com/kickingvegas/casual-calc
+(use-package casual-calc
+  :ensure t)
+
+;; https://github.com/kickingvegas/casual-avy
+(use-package casual-avy
   :ensure t)
