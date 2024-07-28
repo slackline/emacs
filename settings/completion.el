9eb66b82e40cc124afc09841219291f427aeffbe
Neil Shephard <nshephard@protonmail.com>
2024-07-28 07:46:09 +0100

    tidying up redundant files

--- settings/completion.el
+++ settings/completion.el
@@ -1,4 +1,11 @@
 ;;; Completion frameworks
+;;; Configuration of vertico, consult, marginalia, embark and orderless
+;;;
+;;; https://github.com/minad/vertico
+;;; https://github.com/minad/marginalia
+;;; https://github.com/minad/consult
+;;; https://github.com/oantolin/embark
+;;; https://github.com/oantolin/orderless
 
 ;; https://github.com/minad/vertico
 (use-package vertico
