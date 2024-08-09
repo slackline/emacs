5af929e829196545f9ef72ae586282058254638e
Neil Shephard <nshephard@protonmail.com>
2024-08-09 22:10:03 +0100

    osm: better keybinding via C-c o to osm-prefix-map

--- settings/osm.el
+++ settings/osm.el
@@ -3,14 +3,11 @@
 ;;;
 ;;; https://elpa.gnu.org/packages/osm.html#org666a5ba
 (use-package osm
-  :bind (("C-c o h" . osm-home)
-	 ("C-c o s" . osm-search)
-	 ("C-c o v" . osm-server)
-	 ("C-c o t" . osm-goto)
-	 ("C-c o x" . osm-gpx-show)
-	 ("C-c o j" . osm-bookmark-jump))
+  :ensure t
   :init
   :custom
   (osm-server 'default)
   (osm-home '(53.356116 -1.463397 15))
+  :bind
+  (("C-c o" . osm-prefix-map))
   )
