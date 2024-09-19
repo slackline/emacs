71fc6d3981c307195761a86d7e53a3dfe4ce5601
Neil Shephard <nshephard@protonmail.com>
2024-09-19 09:14:26 +0100

    pass: Adding password-store-menu a transient for pass interaction

--- settings/pass.el
+++ settings/pass.el
@@ -1,6 +1,10 @@
 ;;; PASS CONFIGURATION
 ;;; --------------------------------------
-
+;; https://github.com/rjekker/password-store-menu
+(use-package password-store-menu
+  :ensure t
+  :config (password-store-menu-enable)
+  :custom (password-store-menu-key "C-c C-p"))
 ;; https://github.com/NicolasPetton/pass
 (use-package pass
   :ensure t)
