c0495784516311911b7730f61d3eff5adb05b687
Neil Shephard <nshephard@protonmail.com>
2024-09-29 19:47:34 +0100

    exercism: submit and show results function

--- settings/exercism.el
+++ settings/exercism.el
@@ -3,3 +3,12 @@
 (use-package exercism
   :config
   (global-set-key (kbd "C-c x") #'exercism))
+
+;; Submits the current buffer to exercism and opens a temp buffer with the output and some additional information like
+;; the URL of the current practice. Via https://emacs.christianbaeuerlein.com/#orgabe4cc7
+(defun exercism-submit ()
+  (interactive)
+  (with-output-to-temp-buffer "*exercism*"
+    (princ(shell-command-to-string (concat "exercism submit " (buffer-file-name))))
+    )
+  (pop-to-buffer "*exercism*"))
