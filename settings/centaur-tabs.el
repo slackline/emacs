374875dd2d2cbdac8572e973c7f94b3201c86bfb
Neil Shephard <nshephard@protonmail.com>
2024-10-09 15:12:33 +0100

    centaurtabs: custom function to group tabs based on projectile root
    Removes previous function that aimed to group based on mode.

--- settings/centaur-tabs.el
+++ settings/centaur-tabs.el
@@ -46,58 +46,6 @@
 ;; ("C-c t C-<right>" . centaur-tabs-move-current-tab-to-right)
 ;; ("C-c t C-<left>" . centaur-tabs-move-current-tab-to-left))
 
-
-(defun centaur-tabs-buffer-groups ()
-  "`centaur-tabs-buffer-groups' control buffers' group rules.
-;;     Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-;;     All buffer name start with * will group to \"Emacs\".
-;;     Other buffer group by `centaur-tabs-get-group-name' with project name."
-  (list
-   (cond
-    ;;; Attempts to group mastodon buffers, not working 2023-01-02
-    ;; ((string-equal "*mastodon" (substring (buffer-name) 0 8))
-    ;;  "Mastodon")
-    ((or (string-equal "*" (substring (buffer-name) 0 1))
-	 (memq major-mode '(magit-process-mode
-			    magit-status-mode
-			    magit-diff-mode
-			    magit-log-mode
-			    magit-file-mode
-			    magit-blob-mode
- 			    magit-blame-mode)))
-     "Emacs")
-    ;; ((derived-mode-p 'prog-mode) "Editing")
-    ;; ((memq major-mode '(python-mode)) "Python")
-    ;;; Following attempts to split magit buffers out to their own group, not working 2023-01-02
-    ;; ((string-equal "*" (substring (buffer-name) 0 1))
-    ;;  "Emacs")
-    ;; ((string-equal "magit" (substring (buffer-name) 0 4)) "Magit")
-    ;; ((memq major-mode '(magit-process-mode
-    ;; 		            magit-status-mode
-    ;; 		            magit-diff-mode
-    ;; 		            magit-log-mode
-    ;; 		            magit-file-mode
-    ;; 		            magit-blob-mode
-    ;; 		            magit-blame-mode))
-    ;;  "Magit")
-    ;; ((derived-mode-p 'dired-mode) "Dired")
-    ((memq major-mode '(helpful-mode
-                        help-mode))
-     "Help")
-    ((memq major-mode '(org-mode
-			org-agenda-clockreport-mode
-			org-src-mode
-			org-agenda-mode
-			org-beamer-mode
-			org-indent-mode
-			org-bullets-mode
-			org-cdlatex-mode
-			org-agenda-log-mode
-			diary-mode))
-     "OrgMode")
-    (t
-     (centaur-tabs-get-group-name (current-buffer))))))
-
 (defun centaur-tabs-hide-tab (x)
   "Do no to show buffer X in tabs."
   (let ((name (format "%s" x)))
