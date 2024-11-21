b142d377ff43c42eef51069ee008afc9220a4b71
Neil Shephard <nshephard@protonmail.com>
2024-11-21 08:52:21 +0000

    conda: update conda-env-home-directory

--- settings/conda.el
+++ settings/conda.el
@@ -3,5 +3,6 @@
 (use-package conda
   :ensure t
   :init
-  (setq conda-anaconda-home (expand-file-name "~/.miniconda3"))
-  (setq conda-env-home-directory (expand-file-name "~/.miniconda3")))
+  (setq conda-env-autoactivate-mode t)
+  (setq conda-anaconda-home (expand-file-name "~/miniforge3"))
+  (setq conda-env-home-directory (expand-file-name "~/miniforge3")))
