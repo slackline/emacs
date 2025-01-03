ccb0899eea0803583cf609dc511c858a4e2a125d
Neil Shephard <nshephard@protonmail.com>
2025-01-03 09:11:18 +0000

    modes: new file for different modes that don't need lots of config

--- init.el
+++ init.el
@@ -234,6 +234,7 @@
 ;;; Shell Interpreter (has to be called early so that commands are available for hooks)
 (load "~/.config/emacs/settings/shell-interpreter.el")
 (load "~/.config/emacs/settings/misc.el")
+(load "~/.config/emacs/settings/modes.el")
 
 ;;; General editor settings
 ;;(load "~/.config/emacs/settings/activities.el")
