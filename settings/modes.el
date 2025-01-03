0ceec94d7d3ad2c2bc102d7f3b8b4b5688d7c737
Neil Shephard <nshephard@protonmail.com>
2025-01-03 09:11:18 +0000

    modes: new file for different modes that don't need lots of config

---
+++ settings/modes.el
@@ -0,0 +1,9 @@
+;;; Various random modes not worthy of their own configuration
+
+;; https://github.com/OpenSauce04/portage-modes
+(use-package portage-modes
+  :ensure t)
+
+;; https://github.com/nverno/tmux-mode
+(use-package tmux-mode
+  :ensure t)
