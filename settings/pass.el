1bfc83e7b23bc7d6cbb3f9c4ef7aab9aca754091
Neil Shephard <nshephard@protonmail.com>
2025-01-23 17:35:54 +0000

    pass: adding pass-otp

--- settings/pass.el
+++ settings/pass.el
@@ -1,10 +1,13 @@
 ;;; PASS CONFIGURATION
 ;;; --------------------------------------
+;; https://github.com/NicolasPetton/pass
+(use-package pass
+  :ensure t)
 ;; https://github.com/rjekker/password-store-menu
 (use-package password-store-menu
   :ensure t
   :config (password-store-menu-enable)
   :custom (password-store-menu-key "C-c C-p"))
-;; https://github.com/NicolasPetton/pass
-(use-package pass
+;; https://github.com/volrath/password-store-otp.el
+(use-package password-store-otp
   :ensure t)
