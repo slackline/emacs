57588f848325a69bd4f4e898e39e8a34f4c5e82f
Neil Shephard <nshephard@protonmail.com>
2025-03-22 22:14:56 +0000

    casual: change key-binding

--- settings/casual-suite.el
+++ settings/casual-suite.el
@@ -4,7 +4,7 @@
 (use-package casual-suite
   :ensure t
   :bind (:map
-         reb-mode-map ("C-o" . casual-re-builder-tmenu)
+         reb-mode-map ("s-c" . casual-re-builder-tmenu)
          :map
-         reb-lisp-mode-map ("C-o" . casual-re-builder-tmenu)))
+         reb-lisp-mode-map ("s-c" . casual-re-builder-tmenu)))
 ;; :after (re-builder)
