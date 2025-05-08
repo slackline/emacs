861b65024258b0b46fe7a6ae2980e8b169ead2c1
Neil Shephard <nshephard@protonmail.com>
2025-05-08 09:06:03 +0100

    key: replace prefix with meaninfu info

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -14,12 +14,59 @@
 ;; Most things commented out here reside under init.el > (use-package emacs :bind (...))
 ;;
 ;; Simplified Emacs keymap in Emacs 29 (and earlier!) : https://systemcrafters.net/newsletter/sc-news-004.html
+
 ;; https://github.com/justbur/emacs-which-key
 ;; Package is now in Emacs core, not sure any of this is needed
+;;
+
 (use-package which-key
   :defer 0.5
   :config (which-key-mode)
-  (global-set-key (kbd "<f8>") 'which-key-show-major-mode))
+  (global-set-key (kbd "<f8>") 'which-key-show-major-mode)
+  ;; +prefix isn't helpful, lets sort that out via...
+  ;; https://gist.github.com/mmarshall540/a12f95ab25b1941244c759b1da24296d)
+  (which-key-add-key-based-replacements
+   "<f1> 4"        "help-other-win"
+   "<f1>"          "help"
+   "<f2>"          "2-column"
+   "C-c"           "mode-and-user"
+   "C-h 4"         "help-other-win"
+   "C-h"           "help"
+   "C-x 4"         "other-window"
+   "C-x 5"         "other-frame"
+   "C-x 6"         "2-column"
+   "C-x 8"         "insert-special"
+   "C-x C-k C-q"   "kmacro-counters"
+   "C-x C-k C-r a" "kmacro-add"
+   "C-x C-k C-r"   "kmacro-register"
+   "C-x C-k"       "keyboard-macros"
+   "C-x RET"       "encoding/input"
+   "C-x a i"       "abbrevs-inverse-add"
+   "C-x a"         "abbrevs"
+   "C-x n"         "narrowing"
+   "C-x p"         "projects"
+   "C-x r"         "reg/rect/bkmks"
+   "C-x t ^"       "tab-bar-detach"
+   "C-x t"         "tab-bar"
+   "C-x v M"       "vc-mergebase"
+   "C-x v b"       "vc-branch"
+   "C-x v"         "version-control"
+   "C-x w ^"       "window-detach"
+   "C-x w"         "window-extras"
+   "C-x x"         "buffer-extras"
+   "C-x"           "extra-commands"
+   "M-g"           "goto-map"
+   "M-s h"         "search-highlight"
+   "M-s"           "search-map")
+  (with-eval-after-load 'page-ext
+    (which-key-add-key-based-replacements
+     "C-x C-p" "page-extras"))
+  ;; Org-mode provides some additional prefix-keys in `org-mode-map'.
+  (with-eval-after-load 'org
+    (which-key-add-keymap-based-replacements org-mode-map
+      "C-c \""      "org-plot"
+      "C-c C-v"     "org-babel"
+      "C-c C-x"     "org-extra-commands")))
 
 ;; helpful settings
 ;; https://github.com/Wilfred/helpful
