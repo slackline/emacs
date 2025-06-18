79c46b16353a6f03052b4370afcb579af672279b
Neil Shephard <nshephard@protonmail.com>
2025-06-18 09:07:16 +0100

    casual-suite: More config options added
    Still not working as I'd like though!

--- settings/casual-suite.el
+++ settings/casual-suite.el
@@ -6,5 +6,14 @@
   :bind (:map
          reb-mode-map ("s-c" . casual-re-builder-tmenu)
          :map
-         reb-lisp-mode-map ("s-c" . casual-re-builder-tmenu)))
+         reb-lisp-mode-map ("s-c" . casual-re-builder-tmenu)
+         :map
+         ibuffer-mode-map))
+(use-package casual-dired
+  :ensure nil
+  :bind (:map dired-mode-map ("C-o" . causal-dired-tmenu)))
+(use-package casual-ibuffer
+  :ensure nil
+  :after (ibuffer)
+  :bind (:map ibuffer-mode-map ("C-o" . causal-ibuffer-tmenu)))
 ;; :after (re-builder)
