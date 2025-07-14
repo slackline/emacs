39ec0ef36126c10cead58c0f44d5239cb07a68f8
Neil Shephard <nshephard@protonmail.com>
2025-07-14 23:22:03 +0100

    tramp: extra options

--- settings/tramp.el
+++ settings/tramp.el
@@ -2,6 +2,7 @@
 ;; --------------------------------------
 ;; https://emacs.stackexchange.com/questions/47969/trouble-connecting-gnu-emacs-to-work-machine-through-ssh-tramp
 ;; https://emacs.stackexchange.com/a/48089
+;; https://coredumped.dev/2025/06/18/making-tramp-go-brrrr./
 
 (use-package tramp
   :ensure t
@@ -11,6 +12,10 @@
   ;; Set prompt so it doesn't hang
   (setq shell-prompt-pattern '"^[^#$%>\n]*~?[#$%>] *")
   (setq tramp-auto-save-directory "~/.config/emacs/tmp/")
+  (setq remote-file-name-inhibit-locks t)
+  (setq tramp-use-scp-direct-remote-copying t)
+  (setq remote-file-name-inhibit-auto-save-visited t)
+  (setq tramp-copy-size-limit (* 1024 1024)) ;; 1Mb
   ;; Set default usernames for different hosts and a global default.
   (add-to-list 'tramp-default-user-alist
 	       '("ssh" ".*ovh'" "arch") t)
