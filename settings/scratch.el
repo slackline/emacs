3baed9470317ac3b7208d9db7e8a1aac84dfdea5
Neil Shephard <nshephard@protonmail.com>
2025-07-29 16:42:30 +0100

    scratch: adding scratch-plus

--- settings/scratch.el
+++ settings/scratch.el
@@ -4,3 +4,13 @@
 (use-package scratch
   :ensure t
   :bind ("C-c s" . scratch))
+
+;; https://git.sr.ht/~swflint/scratch-plus
+(use-package scratch-plus
+  :ensure t
+  :defer 0.5
+  :hook
+  (add-hook 'after-init-hook #'scratch-plus-mode)
+  :config
+  (setq scratch-plus-save-directory "~/.config/emacs/scratch")
+  (setq scratch-plus-project-subdir ".scratch"))
