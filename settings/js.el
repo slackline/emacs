d11e734205821af97da4f4777a5e6d7cd00ce997
slackline <776695+slackline@users.noreply.github.com>
2025-11-22 20:23:45 +0000

    Deploying to gh-pages from @ slackline/emacs@257fc37c496d293a416838f6e301545b4c32cc8b 🚀

--- settings/js.el
+++
@@ -1,5 +0,0 @@
-;; JAVASCRIPT/JSON CONFIGURATION
-;; --------------------------------------
-(add-to-list 'auto-mode-alist '("\\.js" . js-mode))
-(add-to-list 'auto-mode-alist '("\\.json" . json-mode))
-(add-to-list 'auto-mode-alist '("\\.svelte" . js-ts-mode))
--- settings/mermaid.el
+++
@@ -1,9 +0,0 @@
-;;; Mermaid Mode --- Settings for https://mermaid.js.org/
-;;; https://github.com/abrochard/mermaid-mode/
-(use-package mermaid-mode
-  :ensure t)
-(use-package ob-mermaid
-  :ensure t)
-;;; https://github.com/JonathanHope/mermaid-ts-mode
-(use-package mermaid-ts-mode
-  :ensure t)
