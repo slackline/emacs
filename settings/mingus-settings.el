e365c57b977bf3d1b31704cd8d221604730ec6a4
slackline <776695+slackline@users.noreply.github.com>
2026-03-16 08:07:44 +0000

    Deploying to gh-pages from @ slackline/emacs@b02fae282054ad39530c30ca7b56c98eb900bcb5 🚀

--- settings/mingus-settings.el
+++
@@ -1,9 +0,0 @@
-;;; Mingus CONFIGURATION
-;;; --------------------------------------
-;;;
-;;; GitHub : https://github.com/pft/mingus
-(use-package mingus
-  :ensure t
-  :config
-  (setq mingus-mpd-host '192.168.1.28
-        mingus-mpd-port '6600))
